d219779702d2d814943563c6e1e8c7cf1c80a2b9
Eduardo Díaz <iamedu@gmail.com>
2014-05-31 22:18:57 -0500

    Initial Commit

---
+++ EngageClient/EngageAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EngageAppDelegate.h
+//  EngageClient
+//
+//  Created by Eduardo Díaz on 5/31/14.
+//  Copyright (c) 2014 EngageWall. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EngageAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ EngageClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EngageClient
+//
+//  Created by Eduardo Díaz on 5/31/14.
+//  Copyright (c) 2014 EngageWall. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
